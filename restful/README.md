5fcff540af79b64c347929f2da3ea8fac0d3cad9
lu.hl <lu.hl@alibaba-inc.com>
2018-10-04 14:53:26 +0800

    restful+swagger

---
+++ restful/README.md
@@ -0,0 +1,7 @@
+- 查看RESTful接口定义：http://localhost:9666/api/application.wadl
+- 查看swagger动态文档：http://localhost:9666/api/swagger.json
+- springboot静态web资源：http://localhost:9666/ok.html
+- springboot swagger静态资源：
+  - http://localhost:9666/index.html
+  - 注意要修改index.html的url = "/api/swagger.json";
+- 测试你好接口 curl "http://localhost:9666/api/biz/hello?name=buaa"
--- restful/src/main/java/org/feuyeux/restful/BizApi.java
+++ restful/src/main/java/org/feuyeux/restful/BizApi.java
@@ -2,31 +2,22 @@ package org.feuyeux.restful;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Component;
 
-@Api(value = "业务API", description = "demo项目业务API")
+@Api(value = "业务API")
 @Component
 @Path("biz")
 public class BizApi {
-    /**
-     * 查看RESTful接口定义：http://localhost:9666/api/application.wadl
-     * 查看swagger动态文档：http://localhost:9666/api/swagger.json
-     * springboot静态web资源：http://localhost:9666/ok.html
-     * springboot swagger静态资源：
-     *      http://localhost:9666/index.html
-     *      注意要修改index.html的url = "/api/swagger.json";
-     * 测试你好接口 curl "http://localhost:9666/api/biz/hello?name=buaa"
-     *
-     * @param name
-     * @return
-     */
     @ApiOperation(value = "你好接口", httpMethod = "GET")
     @GET
     @Path("hello")
+    @Produces(MediaType.TEXT_PLAIN)
     public String hello(@QueryParam("name") String name) {
         return "hello " + name;
     }
